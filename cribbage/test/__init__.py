bc454903bfbee52ccfa80caef778a75918ccbb4c
Andrew Enfield <andrewe@microsoft.com>
2022-06-08 20:08:55 -0700

    starting w/ organization that i think'll work for a package and tests.

---
+++ cribbage/test/__init__.py
@@ -0,0 +1,3 @@
+# per https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html#goodpractices
+# pytest -- pyargs cribbage
+# will cause pytest to find mypkg wherever it's installed and collect tests from there
