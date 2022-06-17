07712d14c90cf75b402d4622d54086e8bcd2f0ae
Andrew Enfield <andrewe@microsoft.com>
2022-06-17 08:15:38 +0100

    Added simple Deck.

--- cribbage/cards.py
+++ cribbage/cards.py
@@ -1,6 +1,26 @@
 import collections
 
+# I based some of the cards impl off of ideas and code in the O'Reilly "Fluent Python" book.
+
+# namedtuple gives us a class w/ the named fields - I'll start w/ it and move from it
+# if/when needed (for ex, to impl a way to encapsulate that the count/score for a face card
+# is 10, which is different from the rank of 11-13)
 Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class Deck:
+    ranks = list('A') + [str(n) for n in range(2, 11)] + list('JQK')
+    suits = ['spades', 'diamonds', 'clubs', 'hearts']
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
 
 
 
--- cribbage/test/test_cards.py
+++ cribbage/test/test_cards.py
@@ -1,5 +1,17 @@
 import cards
 
+class TestDeck:
+    def test_deck_has_52_cards(self):
+        sut = cards.Deck()
+        assert len(sut) == 52
+
+    def test_deck_gives_cards(self):
+        sut = cards.Deck()
+        assert sut[0].rank == 'A'
+        assert sut[0].suit == 'spades'
+
 class TestCard:
-    def test_has_rank_and_suit(self):
-        sut = cards.Card('ace', suit='spades')
+    def test_card_has_rank_and_suit(self):
+        sut = cards.Card(rank='K', suit='spades')
+        assert sut.rank == 'K'
+        assert sut.suit == 'spades'
