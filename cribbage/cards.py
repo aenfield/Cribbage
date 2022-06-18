dbd0b676029dc6ccc997efab71d16ed18ee4b47a
Andrew Enfield <andrewe@microsoft.com>
2022-06-18 10:48:16 +0100

    Added ability to shuffle using standard lib.

--- cribbage/cards.py
+++ cribbage/cards.py
@@ -10,7 +10,7 @@ Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class Deck:
     ranks = list('A') + [str(n) for n in range(2, 11)] + list('JQK')
-    suits = ['spades', 'diamonds', 'clubs', 'hearts']
+    suits = ['spades', 'hearts', 'diamonds', 'clubs']
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
@@ -21,6 +21,11 @@ class Deck:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+    # to support random.shuffle
+    def __setitem__(self, key, value):
+        # key is the index position in the deck, value is the Card instance
+        self._cards[key] = value
 
 
 
--- cribbage/test/test_cards.py
+++ cribbage/test/test_cards.py
@@ -1,5 +1,7 @@
 import cards
 
+import random # for shuffle
+
 class TestDeck:
     def test_deck_has_52_cards(self):
         sut = cards.Deck()
@@ -9,6 +11,11 @@ class TestDeck:
         sut = cards.Deck()
         assert sut[0].rank == 'A'
         assert sut[0].suit == 'spades'
+
+    def test_deck_can_be_shuffled(self):
+        sut = cards.Deck()
+        random.shuffle(sut)
+
 
 class TestCard:
     def test_card_has_rank_and_suit(self):
