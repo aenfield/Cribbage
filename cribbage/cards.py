5115914612ad36ba8e27036ffe3cae887fa6ec85
Andrew Enfield <andrewe@microsoft.com>
2022-06-21 11:54:16 +0100

    Added start of Hand class, and from_spec methods for cards and hands.

--- cribbage/cards.py
+++ cribbage/cards.py
@@ -2,15 +2,9 @@ import collections
 
 # I based some of the cards impl off of ideas and code in the O'Reilly "Fluent Python" book.
 
-# namedtuple gives us a class w/ the named fields - I'll start w/ it and move from it
-# if/when needed (for ex, to impl a way to encapsulate that the count/score for a face card
-# is 10, which is different from the rank of 11-13)
-Card = collections.namedtuple('Card', ['rank', 'suit'])
-
-
 class Deck:
     ranks = list('A') + [str(n) for n in range(2, 11)] + list('JQK')
-    suits = ['spades', 'hearts', 'diamonds', 'clubs']
+    suits = list('SHDC')
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
@@ -38,7 +32,21 @@ class Hand:
     def __getitem__(self, position):
         return self._cards[position]
 
+    @staticmethod
+    def from_specs(specs):
+        # cards = []
+        # for spec in specs:
+        #     cards.append(Card.from_spec(spec))
 
-# if __name__ == '__main__':
-#     foo = Card('7', 'spades')
-#     print(foo)
+        return Hand([Card.from_spec(spec) for spec in specs])
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    @staticmethod
+    def from_spec(spec):
+        # 'spec' is a two character string where the rank is the first char and suit the second (S, H, D, or C)
+        return Card(spec[0], spec[1])
+
--- cribbage/test/test_cards.py
+++ cribbage/test/test_cards.py
@@ -10,7 +10,7 @@ class TestDeck:
     def test_deck_gives_cards(self):
         sut = cards.Deck()
         assert sut[0].rank == 'A'
-        assert sut[0].suit == 'spades'
+        assert sut[0].suit == 'S'
 
     def test_deck_can_be_shuffled(self):
         sut = cards.Deck()
@@ -18,19 +18,30 @@ class TestDeck:
 
 
 class TestHand:
-    # TODO can create hand directly by spceifying cards
     def test_can_create_hand_directly(self):
-        sut = cards.Hand([cards.Card('K', 'spades')])
+        sut = cards.Hand([cards.Card('K', 'S')])
         
     def test_hand_has_length(self):
-        sut = cards.Hand([cards.Card('K', 'spades')])
+        sut = cards.Hand([cards.Card('K', 'S')])
         assert len(sut) == 1
     
     def test_hand_can_be_indexed_into(self):
-        sut = cards.Hand([cards.Card('K', 'spades')])
+        sut = cards.Hand([cards.Card('K', 'S')])
         assert sut[0].rank == 'K'
 
-    # TODO can create hand directly by specifying cards as strings
+    def test_can_create_hand_with_multiple_cards_directly(self):
+        sut = cards.Hand([cards.Card('K', 'H'), cards.Card('4', 'S')])
+        assert len(sut) == 2
+        assert sut[1].rank == '4'
+
+    def test_can_create_multicard_hand_with_specs(self):
+        sut = cards.Hand.from_specs(['4H', 'KS'])
+        assert len(sut) == 2
+        assert sut[0].rank == '4'
+        assert sut[0].suit == 'H'
+        assert sut[1].rank == 'K'
+        assert sut[1].suit == 'S'
+
     # TODO can get hand from deck, siwth size param - check six and one (for cut card)
     # TODO can score hand - size four, with cut card (perhaps provide handy method to add card, or just manually combine?)
     # TODO for scoring, support 15s, runs, flushes, pairs, nobs
@@ -38,9 +49,13 @@ class TestHand:
 
 class TestCard:
     def test_card_has_rank_and_suit(self):
-        sut = cards.Card(rank='K', suit='spades')
+        sut = cards.Card(rank='K', suit='S')
         assert sut.rank == 'K'
-        assert sut.suit == 'spades'
+        assert sut.suit == 'S'
 
-    # TODO can create via short form, like '4H' for four of hearts
+    def test_can_create_card_with_shortform_text(self):
+        sut = cards.Card.from_spec('4H')
+        assert sut.rank == '4'
+        assert sut.suit == 'H'
+
     # TODO get short form representation (as str?) using either SHDC or Unicode symbols
